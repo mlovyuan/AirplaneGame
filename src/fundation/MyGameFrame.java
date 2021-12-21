a16789c83dcb7fcd5b9de2703f5fb66142a7659c
mlovyuan <mlovyuan@yahoo.com.tw>
2021-12-21 23:02:09 +0800

    Create frame window

---
+++ src/fundation/MyGameFrame.java
@@ -0,0 +1,26 @@
+package fundation;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MyGameFrame extends Frame {
+    public void initFrame() {
+        setTitle("Airplane Game");
+        setVisible(true);
+        setSize(500, 500);
+        setLocation(300, 300);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        MyGameFrame frame = new MyGameFrame();
+        frame.initFrame();
+    }
+}
