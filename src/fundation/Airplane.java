956cb31bddc0958de58433646d687720ec7958db
mlovyuan <mlovyuan@yahoo.com.tw>
2021-12-25 20:23:35 +0800

    Use Airplane class in MyGameFrame and make it move horizontal line

--- src/fundation/Airplane.java
+++ src/fundation/Airplane.java
@@ -3,13 +3,24 @@ package fundation;
 import java.awt.*;
 
 public class Airplane extends GameObject {
+
+    private boolean _isLeftDirection;
+
     public Airplane(Image img, int xAxis, int yAxis, int speed) {
-        super(img, xAxis, yAxis, speed);
+        super(img, xAxis, yAxis, speed,30,30);
     }
 
     @Override
     public void drawMyself(Graphics graphics) {
         super.drawMyself(graphics);
-        _xAxis += 3;
+        if (_isLeftDirection)
+            _xAxis -= _speed;
+        else
+            _xAxis += _speed;
+
+        if (_xAxis > GameUtil._frame_width - _width)
+            _isLeftDirection = true;
+        else if (_xAxis < 0)
+            _isLeftDirection = false;
     }
 }
--- src/fundation/MyGameFrame.java
+++ src/fundation/MyGameFrame.java
@@ -7,37 +7,14 @@ import java.awt.event.WindowEvent;
 import static fundation.GameUtil.*;
 
 public class MyGameFrame extends Frame {
-    Image _background;
-    Image _airplane;
-
-    public void initFrame() {
-        setTitle("Airplane Game");
-        setVisible(true);
-        setSize(_frame_width, _frame_height);
-        setLocation(300, 300);
-
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
-
-        _background = getImage("images/starry.jpg");
-        _airplane = getImage("images/aircraft.png");
-
-        // start paint thread
-        new PaintThread().start();
-    }
-
-    int x = 200, y = 200;
+    private Image _backgroundImg = getImage("images/starry.jpg");
+    private Image _airplaneImg = getImage("images/aircraft.png");
+    private Airplane airplane = new Airplane(_airplaneImg, 200, 200, 3);
 
     @Override
     public void paint(Graphics g) {
-        g.drawImage(_background, 0, 0, _frame_width, _frame_height, null);
-        g.drawImage(_airplane, x, y, 30, 30, null);
-        x--;
-        y--;
+        g.drawImage(_backgroundImg, 0, 0, _frame_width, _frame_height, null);
+        airplane.drawMyself(g);
     }
 
     class PaintThread extends Thread {
@@ -54,15 +31,31 @@ public class MyGameFrame extends Frame {
         }
     }
 
-    private Image offScreenImage = null;
-
     // resolve game window flicker
     public void update(Graphics g) {
+        Image offScreenImage = null;
         if (offScreenImage == null)
             offScreenImage = createImage(_frame_width, _frame_height);
         Graphics gOff = offScreenImage.getGraphics();
         paint(gOff);
         g.drawImage(offScreenImage, 0, 0, null);
+    }
+
+    public void initFrame() {
+        setTitle("Airplane Game");
+        setVisible(true);
+        setSize(_frame_width, _frame_height);
+        setLocation(300, 300);
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        // start paint thread
+        new PaintThread().start();
     }
 
     public static void main(String[] args) {
